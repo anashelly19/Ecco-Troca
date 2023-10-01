bf566dd44517ef3f18fd01f1d3099bf336b7f5a8
anash <anashellypires57@gmail.com>
2023-10-01 17:28:01 -0300

    Iniciando a segunda tela

---
+++ app/src/main/java/com/projeto/ecotroca/formCadastro.java
@@ -0,0 +1,14 @@
+package com.projeto.ecotroca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class formCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_cadastro);
+    }
+}
--- app/src/main/java/com/projeto/ecotroca/formLogin.java
+++ app/src/main/java/com/projeto/ecotroca/formLogin.java
@@ -2,9 +2,13 @@ package com.projeto.ecotroca;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class formLogin extends AppCompatActivity {
+    private TextView text_tela_cadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +16,18 @@ public class formLogin extends AppCompatActivity {
         setContentView(R.layout.activity_form_login);
 
         getSupportActionBar().hide();
+        IniciarComponentes();
+
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(formLogin.this,formCadastro.clas);
+                startActivity(intent);
+            }
+        });
+    }
+    private void IniciarComponentes(){
+        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
     }
 }
